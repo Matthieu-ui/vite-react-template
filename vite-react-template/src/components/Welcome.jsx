acf9278136bd75516def33f91a19a5972b3d8ce6
matthieu-ui <mattfelker206@outlook.com>
2022-06-15 22:44:50 -0700

    react/vite blank templete

---
+++ vite-react-template/src/components/Welcome.jsx
@@ -0,0 +1,13 @@
+
+export default function Welcome() {
+    return (
+      <>
+        <div className="wrapper">
+          <h1>Welcome To My Template</h1>
+          <p>You Are Now Set Up With a React and Vite</p>
+      
+        </div>
+      </>
+    );
+  }
+  
